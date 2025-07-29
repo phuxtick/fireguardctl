8f25cdca98fa7c318cc99ff26d756d3844b3918d
Shawn Andrews <andrews.shawn@gmail.com>
2025-07-28 21:33:11 -0500

    fix models

--- app/models.py
+++ app/models.py
@@ -16,5 +16,7 @@ class HostStatus(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     hostname: str
     location: str
-    services: str  # We'll store this as a JSON string for now
+    services: str  # JSON string of metrics
+    interfaces: Optional[str] = None  # <- ADD THIS
+    system_info: Optional[str] = None  # <- AND THIS
     timestamp: datetime
